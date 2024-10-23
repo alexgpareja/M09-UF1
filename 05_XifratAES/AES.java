2ed0f56cb133299bb82c7b3f793f2794c1c864e7
alex <2023_Alex.Gil@iticbcn.cat>
2024-10-23 19:32:37 +0200

    exercici aes completat

--- 05_XifratAES/AES.java
+++ 05_XifratAES/AES.java
@@ -1,6 +1,9 @@
 import javax.crypto.spec.*;
 import javax.crypto.Cipher; // S'utilitza per fer operacions de xifratge/desxifratge
+
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.Arrays;
 
 public class AES {
 
@@ -17,16 +20,20 @@ public class AES {
         byte[] byteMsg = msg.getBytes("UTF-8");
 
         // Omplir el iv amb valors randomSecure
-        SecureRandom secureRandom = new SecureRandom();     // Crea una instancia de numeros aleatoris
-        secureRandom.nextBytes(iv);     // Omple l'array de iv amb valors aleatoris
+        SecureRandom secureRandom = new SecureRandom(); // Crea una instancia de numeros aleatoris
+        secureRandom.nextBytes(iv); // Omple l'array de iv amb valors aleatoris
 
         // Genera IvParameterSpec
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);      // Crea un objecte que encapsula el iv, per passar-lo al xifratge
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv); // Crea un objecte que encapsula el iv, per passar-lo
+                                                                   // al xifratge
 
         // Genera hash
-        MessageDigest messageDigest = MessageDigest.getInstance(ALGORISME_HASH);    // Es crea una instància de l'algoritme SHA-256 per fer un hash de la clau
-        byte[] clauHash = messageDigest.digest(clau.getBytes());    // Aplica el hash SHA-256 a la contrasenya
-        SecretKeySpec secretKeySpec = new SecretKeySpec(clauHash, ALGORISME_XIFRAT);    // Es crea un objecte que conté la clau AES generada a partir del hash de la contrasenya.
+        MessageDigest messageDigest = MessageDigest.getInstance(ALGORISME_HASH); // Es crea una instància de l'algoritme
+                                                                                 // SHA-256 per fer un hash de la clau
+        byte[] clauHash = messageDigest.digest(clau.getBytes()); // Aplica el hash SHA-256 a la contrasenya
+        SecretKeySpec secretKeySpec = new SecretKeySpec(clauHash, ALGORISME_XIFRAT); // Es crea un objecte que conté la
+                                                                                     // clau AES generada a partir del
+                                                                                     // hash de la contrasenya.
 
         // Encrypt.
         Cipher cipher = Cipher.getInstance(FORMAT_AES);
@@ -35,9 +42,8 @@ public class AES {
 
         // Combinar IV i part xifrada.
         byte[] ivMsgXifrat = new byte[MIDA_IV + msgXifrat.length];
-        System.arraycopy(iv, 0, ivMsgXifrat, 0, msgXifrat.length);
-        
-
+        System.arraycopy(iv, 0, ivMsgXifrat, 0, MIDA_IV);
+        System.arraycopy(msgXifrat, 0, ivMsgXifrat, MIDA_IV, msgXifrat.length);
 
         // return iv+msgxifrat
         return ivMsgXifrat;
@@ -46,14 +52,28 @@ public class AES {
     public static String desxifraAES(byte[] bIvIMsgXifrat, String clau) throws Exception {
 
         // Extreure l'IV.
+        byte[] iv = Arrays.copyOfRange(bIvIMsgXifrat, 0, MIDA_IV);
 
         // Extreure la part xifrada.
+        byte[] msgXifrat = Arrays.copyOfRange(bIvIMsgXifrat, MIDA_IV, bIvIMsgXifrat.length);
 
         // Fer hash de la clau
+        MessageDigest digest = MessageDigest.getInstance(ALGORISME_HASH);
+        byte[] clauHash = digest.digest(clau.getBytes(StandardCharsets.UTF_8));
+        SecretKeySpec secretKey = new SecretKeySpec(clauHash, ALGORISME_XIFRAT);
 
-        // Desxifrar.
+        // Generar IvParameterSpec
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
 
-        // return String desxifrat
+        // Inicialitzar el desxifrat
+        Cipher cipher = Cipher.getInstance(FORMAT_AES);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+
+        // Desxifrar el missatge
+        byte[] msgDesxifrat = cipher.doFinal(msgXifrat);
+
+        // Retornar el missatge desxifrat com a String
+        return new String(msgDesxifrat, StandardCharsets.UTF_8);
     }
 
     public static void main(String[] args) {
